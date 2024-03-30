05fc0bc5281347850c73fe8087208f5b1a4c2056
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-03-30 23:03:36 +0900

    UI Components 코드 연결

---
+++ LEDBoard/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  LEDBoard
+//
+//  Created by 유림 on 3/30/24.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var purpleButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blackButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tapTextColorButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
+    }
+    
+    
+}
